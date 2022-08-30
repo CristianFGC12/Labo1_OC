ab96789e4e5002cf50d2914910770bc470354c87
CristianFGC12 <90874820+CristianFGC12@users.noreply.github.com>
2022-08-29 18:26:03 -0600

    Add files via upload

---
+++ LABORATORIO_01_EDII/Arbol/Class1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol
+{
+    public class AVLTree<T>
+    {
+        private class Nodo
+        {
+            public T value { get; set; }
+            public int height { get; set; }
+            public Nodo left { get; set; }
+            public Nodo right { get; set; }
+            public Nodo(T _value)
+            {
+                value = _value;
+                height = 1;
+            }
+        }
+        private Nodo root;
+        public delegate bool compareFuncion(T value, string boolOperator, T value2);
+        public List<T> getAll()
+        {
+            List<T> newList = new List<T>();
+            getInOrder(root, newList);
+            return newList;
+        }
+        private void getInOrder(Nodo node, List<T> allData)
+        {
+            if (node != null)
+            {
+                getInOrder(node.left, allData);
+                allData.Add(node.value);
+                getInOrder(node.right, allData);
+            }
+        }
+        public void insert(T value, compareFuncion compare)
+        {
+            root = insertNode(root, value, compare);
+        }
+        private int max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+        private int height(Nodo node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            return node.height;
+        }
+        private int getBalanceFactor(Nodo node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            return height(node.left) - height(node.right);
+        }
+        private Nodo rotateRight(Nodo y)
+        {
+            Nodo x = y.left;
+            Nodo T2 = x.right;
+            x.right = y;
+            y.left = T2;
+            y.height = max(height(y.left), height(y.right)) + 1;
+            x.height = max(height(x.left), height(x.right)) + 1;
+
+            return x;
+        }
+        private Nodo rotateLeft(Nodo x)
+        {
+            Nodo y = x.right;
+            Nodo T2 = y.left;
+            y.left = x;
+            x.right = T2;
+            x.height = max(height(x.left), height(x.right)) + 1;
+            y.height = max(height(y.left), height(y.right)) + 1;
+
+            return y;
+        }
+        private Nodo insertNode(Nodo node, T value, compareFuncion compare)
+        {
+            if (node == null)
+            {
+                return new Nodo(value);
+            }
+
+            if (compare(value, "<", node.value))
+            {
+                node.left = insertNode(node.left, value, compare);
+            }
+            else if (compare(value, ">", node.value))
+            {
+                node.right = insertNode(node.right, value, compare);
+            }
+            else
+            {
+                return node;
+            }
+
+            node.height = max(height(node.left), height(node.right)) + 1;
+            int balanceFactor = getBalanceFactor(node);
+            if (balanceFactor > 1)
+            {
+                if (compare(value, "<", node.left.value))
+                {
+                    return rotateRight(node);
+                }
+                else if (compare(value, ">", node.left.value))
+                {
+                    node.left = rotateLeft(node.left);
+                    return rotateRight(node);
+                }
+            }
+            if (balanceFactor < -1)
+            {
+                if (compare(value, ">", node.right.value))
+                {
+                    return rotateLeft(node);
+                }
+                else if (compare(value, "<", node.right.value))
+                {
+                    node.right = rotateRight(node.right);
+                    return rotateLeft(node);
+                }
+            }
+
+            return node;
+        }
+
+        private int depth(Nodo root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+
+            return 1 + Math.Max(depth(root.left), depth(root.right));
+        }
+
+        public int getDepth()
+        {
+            return depth(root);
+        }
+
+        private T Busqueda(T value, compareFuncion compare, Nodo raiz)
+        {
+            if (raiz == null)
+            {
+                return default(T);
+            }
+
+            if (compare(value, "==", raiz.value))
+            {
+                return raiz.value;
+            }
+
+            if (compare(value, "<", raiz.value))
+            {
+                return Busqueda(value, compare, raiz.left);
+            }
+
+            if (compare(value, ">", raiz.value))
+            {
+                return Busqueda(value, compare, raiz.right);
+            }
+
+            return default(T);
+        }
+
+        public T Search(T value, compareFuncion compare)
+        {
+            return Busqueda(value, compare, root);
+        }
+
+        //Sort
+        public void Sort(compareFuncion compare)
+        {
+            List<T> datosarbol = getAll();
+            root = null;
+            foreach (T value in datosarbol)
+            {
+                insert(value, compare);
+            }
+        }
+
+        private Nodo BusquedaNodo(T value, compareFuncion compare, Nodo raiz)
+        {
+            if (raiz == null)
+            {
+                return null;
+            }
+
+            if (compare(value, "==", raiz.value))
+            {
+                return raiz;
+            }
+
+            if (compare(value, "<", raiz.value))
+            {
+                return BusquedaNodo(value, compare, raiz.left);
+            }
+
+            if (compare(value, ">", raiz.value))
+            {
+                return BusquedaNodo(value, compare, raiz.right);
+            }
+
+            return null;
+        }
+        private Nodo Replace(T value, T nuw, compareFuncion compare, Nodo raiz)
+        {
+            Nodo Cambio = BusquedaNodo(value, compare, root);
+            if (Cambio == null)
+            {
+                return null;
+            }
+            Cambio.value = nuw;
+            return Cambio;
+        }
+        public void Reemplazar(T value, T nuw, compareFuncion compare)
+        {
+            Replace(value, nuw, compare, root);
+        }
+    }
+}
---
+++ LABORATORIO_01_EDII/LABORATORIO_01_EDII/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LABORATORIO_01_EDII.Model;
+using Arbol;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LABORATORIO_01_EDII
+{
+    class Program
+    {
+        //private static string _path = @"C:\Users\Usuario\Documents\Trabajador.json";
+        public static AVLTree<Trabajador> trabajador = new AVLTree<Trabajador>();
+        static void Main(string[] args)
+        {
+            int con = 0;
+            do
+            {
+                int opcion = 0;
+                Console.WriteLine("Seleccione la opción que quiera realizar");
+                Console.WriteLine("1. Subir Archivo");
+                Console.WriteLine("2. Agregar Manual");
+                Console.WriteLine("3. Editar Manual");
+                Console.WriteLine("4. Eliminar Manual");
+                Console.WriteLine("5. Busqueda");
+                Console.WriteLine("A la hora de ingresar datos siga el siguiente formato "+'"'+'"'+"dato"+'"'+'"');
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        string ruta = "";
+                        Console.WriteLine("Ingrese la ruta del archivo: ");
+                        ruta = Console.ReadLine();
+                        var reader = new StreamReader(File.OpenRead(ruta));
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var value = line.Split(';');
+                            if (value[0] == "INSERT")
+                            {
+                                var data = JsonConvert.DeserializeObject<Trabajador>(value[1]);
+                                Trabajador trabajar = data;
+                                trabajador.insert(trabajar, ComparacioDPI);
+
+                            }
+                            else if (value[0] == "PATCH")
+                            {
+                                var data = JsonConvert.DeserializeObject<Trabajador>(value[1]);
+                                Trabajador trabajar = data;
+                                if (trabajador.Search(trabajar, ComparacioDPI).name == trabajar.name)
+                                {
+                                    if (trabajar.dateBirth != null)
+                                    {
+                                        trabajador.Search(trabajar, ComparacioDPI).dateBirth = trabajar.dateBirth;
+                                    }
+                                    if(trabajar.address != null)
+                                    {
+                                        trabajador.Search(trabajar, ComparacioDPI).address = trabajar.address;
+                                    }
+                    
+                                }
+                            }
+                            else if (value[0] == "DELETE")
+                            {
+                                var data = JsonConvert.DeserializeObject<Trabajador>(value[1]);
+                                Trabajador trabajar = data;
+                                List<Trabajador> trabajo = trabajador.getAll();
+                                int cant = trabajo.Count();
+                                for (int i = 0; i < trabajo.Count; i++)
+                                {
+                                    if (trabajo[i].name == trabajar.name && trabajo[i].dpi == trabajar.dpi)
+                                    {
+                                        trabajo.RemoveAt(i);
+                                    }
+                                }
+                                trabajador = new AVLTree<Trabajador>();
+                                int cant2 = trabajo.Count();
+                                for (int j = 0; j < trabajo.Count; j++)
+                                {
+                                    trabajador.insert(trabajo[j], ComparacioDPI);
+                                }
+                            }
+                        }
+                        Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                        con = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Trabajador trabaja = new Trabajador();
+                        Console.WriteLine("Ingrese el nombre del solicitante");
+                        trabaja.name = Console.ReadLine();
+                        Console.WriteLine("Ingrese el dpi del solicitante");
+                        trabaja.dpi = Console.ReadLine();
+                        Console.WriteLine("Ingrese la fecha de nacimiento del solicitante");
+                        trabaja.dateBirth = Console.ReadLine();
+                        Console.WriteLine("Ingrese la dirección del solicitante");
+                        trabaja.address = Console.ReadLine();
+                        trabajador.insert(trabaja, ComparacionNombre);
+                        Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                        con = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Trabajador trabaja2 = new Trabajador();
+                        trabajador.Sort(ComparacioDPI);
+                        string nombre;
+                        string editar;
+                        Console.WriteLine("Ingrese el nombre del solicitante");
+                        nombre = Console.ReadLine();
+                        Console.WriteLine("Ingrese el dpi del solicitante");
+                        trabaja2.dpi = Console.ReadLine();
+                        Console.WriteLine("¿Que desea editar?");
+                        editar = Console.ReadLine();
+                        if(editar == "Nacimiento") 
+                        {
+                            Console.WriteLine("Ingrese la fecha de nacimiento del solicitante");
+                            if (trabajador.Search(trabaja2, ComparacioDPI).name == nombre)
+                            {
+                                trabajador.Search(trabaja2, ComparacioDPI).dateBirth = Console.ReadLine();
+                            }
+                            else 
+                            {
+                                Console.WriteLine("Solictante no encontrado");
+                                Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                                con = Convert.ToInt32(Console.ReadLine());
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        else if(editar == "Direccion") 
+                        {
+                            Console.WriteLine("Ingrese direccion del solicitante");
+                            if (trabajador.Search(trabaja2, ComparacioDPI).name == nombre)
+                            {
+                                trabajador.Search(trabaja2, ComparacioDPI).address = Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Solictante no encontrado");
+                                Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                                con = Convert.ToInt32(Console.ReadLine());
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        else 
+                        {
+                            Console.WriteLine("Atributo no valido");
+                            Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                            con = Convert.ToInt32(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                        con = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 4:
+                        string nombre2;
+                        Trabajador solic = new Trabajador();
+                        Console.WriteLine("Ingrese el nombre del solicitante");
+                        nombre2 = Console.ReadLine();
+                        Console.WriteLine("Ingrese el dpi del solicitante");
+                        solic.dpi= Console.ReadLine();
+                        if(trabajador.Search(solic, ComparacioDPI)== null || trabajador.Search(solic, ComparacioDPI).name != nombre2) 
+                        {
+                            Console.WriteLine("Solictante no encontrado");
+                            Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                            con = Convert.ToInt32(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        List<Trabajador> soli = trabajador.getAll();
+                        for (int i = 0; i < soli.Count; i++) 
+                        {
+                            if(soli[i].name == nombre2 && soli[i].dpi == solic.dpi) 
+                            {
+                                soli.RemoveAt(i);
+                            }
+                        }
+                        trabajador = new AVLTree<Trabajador>();
+                        for(int j = 0; j < soli.Count; j++) 
+                        {
+                            trabajador.insert(soli[j], ComparacioDPI);
+                        }
+                        Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                        con = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 5:
+                        string ident;
+                        string path;
+                        Console.WriteLine("Ingrese el nombre a buscar");
+                        ident = Console.ReadLine();
+                        trabajador.Sort(ComparacioDPI);
+                        List<Trabajador> total = trabajador.getAll();
+                        List<Trabajador> result = new List<Trabajador>();
+                        int tot = total.Count;
+                        for(int i = 0; i < tot; i++) 
+                        {
+                            if(total[i].name == ident) 
+                            {
+                                result.Add(total[i]);
+                            }
+                        }
+                        if(result.Count <= 0) 
+                        {
+                            Console.WriteLine("No hay coincidencias");
+                            Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                            con = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        }
+                        Console.WriteLine("Ingrese la ruta y el nombre de como se guardar como .json");
+                        path = Console.ReadLine();
+                        Serializacion(result,path);
+                        Console.WriteLine("Desea volver al menu No = 0, Si = 1");
+                        con = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                }
+            } while (con == 1);
+        }
+        public static void Serializacion(List<Trabajador> Lista, string path) 
+        {
+            string solictanteJson = JsonConvert.SerializeObject(Lista.ToArray(),Formatting.Indented);
+            File.WriteAllText(path, solictanteJson);
+        }
+        public static bool ComparacionNombre(Trabajador paciente, string operador, Trabajador paciente2)
+        {
+            int Comparacion = String.Compare(paciente.name, paciente2.name);
+            if (operador == "<")
+            {
+                return Comparacion < 0;
+            }
+            else if (operador == ">")
+            {
+                return Comparacion > 0;
+            }
+            else if (operador == "==")
+            {
+                return Comparacion == 0;
+            }
+            else return false;
+        }
+        public static bool ComparacioDPI(Trabajador paciente, string operador, Trabajador paciente2)
+        {
+            int Comparacion = String.Compare(paciente.dpi, paciente2.dpi);
+            if (operador == "<")
+            {
+                return Comparacion < 0;
+            }
+            else if (operador == ">")
+            {
+                return Comparacion > 0;
+            }
+            else if (operador == "==")
+            {
+                return Comparacion == 0;
+            }
+            else return false;
+        }
+    }
+}
